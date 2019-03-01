c3f49a86763114c21b220b137f7d0f5fdd270d50
Abner <axocop@ufm.edu>
2019-03-01 01:57:10 -0600

    finishing java classes

--- src/main/java/Nacimiento.java
+++ src/main/java/Nacimiento.java
@@ -1,14 +1,27 @@
 
 public class Nacimiento {
 
-    public static void val(String na){
-        int x =0;
+    String aviso6;
+
+    public  void val(String na){
+        int x;
         x = na.length();
 
 
         if(x > 30)
         {
             System.out.println("El lugar ingresado es demasiado largo");
+            aviso6 = "El lugar ingresado es demasiado largo";
+        }
+
+        else if (x == 0){
+            System.out.println("Debe ingresar el lugar de nacimiento");
+            aviso6 = "Debe ingresar el lugar de nacimiento";
+        }
+
+        else{
+            System.out.println("Lugar de nacimiento guardado correctamente");
+            aviso6 = "Lugar de nacimiento guardado correctamente";
         }
     }
 
